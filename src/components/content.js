f64162a4a2c56e4d60ddbfccd3399caf231a4f2f
leadingmind <leadingmind@users.noreply.github.com>
2021-07-07 14:24:52 +0530

    Content update

--- src/components/content.js
+++ src/components/content.js
@@ -1,3 +1,21 @@
-export default ()=>(
-  <p>There will be the content</p>
-);
+import {Grid,Paper} from "@material-ui/core";
+
+import SingleItem from "./singleitem";
+
+export default function Content() {
+  return (
+    <div>
+      
+      <Grid 
+        container 
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item lg={10} sm={12} xs={12}>
+          <SingleItem />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}

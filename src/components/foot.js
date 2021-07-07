cd2476ce56c14f9f08f9b36f91ae4559edd75572
leadingmind <leadingmind@users.noreply.github.com>
2021-07-07 11:15:29 +0530

    Container update

--- src/components/foot.js
+++ src/components/foot.js
@@ -1,46 +1,51 @@
 // import {styled} from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import ShopIcon from '@material-ui/icons/Shop';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import TurnedInNotIcon from '@material-ui/icons/TurnedInNot';
-
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import ShopIcon from "@material-ui/icons/Shop";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import TurnedInNotIcon from "@material-ui/icons/TurnedInNot";
+import Grid from "@material-ui/icons"
 const useStyles = makeStyles((theme) => ({
   button: {
-    margin: theme.spacing(1),
-  },
+    margin: theme.spacing(1)
+  }
 }));
 
-export default function Foot(){
+export default function Foot() {
   const classes = useStyles();
 
   return (
-    <div>
-      <Button
+    <Grid container spacing={0}>
+      <Grid item xs={4}>
+        <Button
           variant="contained"
           color="secondary"
           className={classes.button}
           startIcon={<TurnedInNotIcon />}
         >
           Save for Later
-    </Button>
-    <Button
-      variant="contained"
-      color="secondary"
-      className={classes.button}
-      startIcon={<AddShoppingCartIcon />}
-    >
-      Add to Cart
-    </Button>
-    <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            startIcon={<ShopIcon />}
-          >
-            Buy Now
-    </Button>
-    </div>
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          startIcon={<AddShoppingCartIcon />}
+        >
+          Add to Cart
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          startIcon={<ShopIcon />}
+        >
+          Buy Now
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
-  
